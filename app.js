f44679bd83aa7dc6cc77fa022fb5e61a2f35796d
najam3 <100590374+najam3@users.noreply.github.com>
2023-08-06 21:37:40 +0500

    commits

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
                 const jwt = require('jsonwebtoken');
                 const allEventsModel = require('./models/allEvents');
                 require('dotenv').config();
-                const port = process.env.port || 3000;
+                const port = process.env.PORT || 3000;
                 
                 // Connect database
 
