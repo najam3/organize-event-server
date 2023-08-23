51705f88158ad897508afb16ef1e9e7dc08a7850
najam3 <100590374+najam3@users.noreply.github.com>
2023-08-24 03:28:14 +0500

    .

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
                   }
 
                   app.use((req, res, next) => {
-                    res.setHeader('Access-Control-Allow-Origin', 'https://dull-pink-dolphin-gear.cyclic.cloud');
+                    res.setHeader('Access-Control-Allow-Origin', 'https://event-frontend-sigma.vercel.app/');
 
                     next();
                   })
