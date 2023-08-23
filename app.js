880b16c299448b5c9634772914c2f1f5b07b4656
najam3 <100590374+najam3@users.noreply.github.com>
2023-08-24 03:34:08 +0500

    .

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
                   }
 
                   app.use((req, res, next) => {
-                    res.setHeader('Access-Control-Allow-Origin', 'https://event-frontend-sigma.vercel.app/');
+                    res.setHeader('Access-Control-Allow-Origin', 'https://event-frontend-sigma.vercel.app');
 
                     next();
                   })
